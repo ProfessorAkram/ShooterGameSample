3b2003f92acd0cf73cf16df512139c7c988fc01b
Taghavi-Burris <aab2515@utulsa.edu>
2021-09-13 16:41:31 -0500

    Player Object Created
    Setup player object
    Started PlayerController script

---
+++ ShooterGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+/**** 
+ * Created by : Stu Dent
+ * Date Created: Sept 13, 2021
+ * 
+ * Last Edited by: 
+ * Last Updated: Sept 13, 2021
+ * 
+ * Description: Player control movements
+ ****/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
